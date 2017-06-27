c82f2cebfba6128c1c58fda3eece42b88ba28fa3
Javier <javier.zunzunegui.b@gmail.com>
2017-06-27 12:01:14 +0100

    chef 11 fix metadata.rb: source_url and issues_url

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures Prometheus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.6.3'
-source_url 'https://github.com/elijah/chef-prometheus'
-issues_url 'https://github.com/elijah/chef-prometheus/issues'
+source_url 'https://github.com/elijah/chef-prometheus' if respond_to?(:source_url)
+issues_url 'https://github.com/elijah/chef-prometheus/issues' if respond_to?(:issues_url)
 
 %w(ubuntu debian centos redhat fedora).each do |os|
   supports os
